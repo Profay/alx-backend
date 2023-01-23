51e613211842ad56232c460ec765cb3a14fca04e
PROFAY <85462029+Profay@users.noreply.github.com>
2023-01-23 23:03:27 +0100

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env python3
+""" Simple helper function module
 """
-This module contains a function that takes two integer arguments page and
-page_size and returns a tuple of size two containing start and end index"
-"""
-
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """this method takes two args and returns tuple of size two
-    containing a start and end index
-    """
-    start_index = 0
-    end_index = 0
-    for i in range(page):
-        start_index = end
-        end_index += page_size
-
+    """ Returns a tuple """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
     return (start_index, end_index)
